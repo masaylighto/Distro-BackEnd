0c8d80ee78f62cc3f4a143a85767c1e221c9d219
Masaylighto <www.masaylighto@gmail.com>
2022-07-02 21:44:33 +0300

    orginize dashboar

--- Distro-back/polls/admin.py
+++ Distro-back/polls/admin.py
@@ -4,10 +4,18 @@ from django.contrib import admin
 
 from polls.models import *
 
+class FeaturesAdmin(admin.ModelAdmin):
+    list_display = ("name", "description","language")
+class TranslationAdmin(admin.ModelAdmin):
+    list_display = ("page_name", "key","value","language")
+class linksAdmin(admin.ModelAdmin):
+    list_display = ("name", "link","language")
+class DistroAdmin(admin.ModelAdmin):
+    list_display = ("name", "Desk","link")
 # Register your models here.
 
-admin.site.register(Translation)
-admin.site.register(links)
-admin.site.register(Distro)
-admin.site.register(Features)
+admin.site.register(Translation,TranslationAdmin)
+admin.site.register(links,linksAdmin)
+admin.site.register(Distro,DistroAdmin)
+admin.site.register(Features,FeaturesAdmin)
 
